9e44a5a54f8350fedf301418ce3e70ddc3db404b
Adrian Block <hello@thisisablock.com>
2021-07-22 17:16:06 +0200

    extended interface for handling identifier

--- src/Contracts/NovaShopEngineInterface.php
+++ src/Contracts/NovaShopEngineInterface.php
@@ -8,7 +8,7 @@ use SSB\Api\Contracts\ShopEngineSettingsInterface;
 interface NovaShopEngineInterface {
 
     public function shopCurrency() : string;
-    public function shopEngineSettings() : ShopEngineSettingsInterface;
+    public function shopEngineSettings(string $shopIdentifier = null) : ShopEngineSettingsInterface;
     public function shopEngineClient(ShopEngineSettingsInterface $settings = null): Client;
     public function shopRegion() : string;
 
