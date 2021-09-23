441f46e62d6bff08fa3c967c6b328c4dbab9f868
Adrian Block <hello@thisisablock.com>
2021-09-23 11:49:28 +0200

    added struct types

---
+++ src/Structs/ArrayStruct.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ShopEngine\Nova\Structs;
+
+class ArrayStruct extends Struct implements \ArrayAccess
+{
+    /**
+     * @var array
+     */
+    protected $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function has(string $property): bool
+    {
+        return \array_key_exists($property, $this->data);
+    }
+
+    public function offsetExists($offset)
+    {
+        return \array_key_exists($offset, $this->data);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->data[$offset]);
+    }
+
+    public function get(string $key)
+    {
+        return $this->offsetGet($key);
+    }
+
+    /**
+     * @param string|int $key
+     * @param array      $value
+     *
+     * @return array
+     */
+    public function set($key, $value)
+    {
+        return $this->data[$key] = $value;
+    }
+
+    public function assign(array $options) : Struct
+    {
+        $this->data = array_replace_recursive($this->data, $options);
+
+        return $this;
+    }
+
+    public function all()
+    {
+        return $this->data;
+    }
+
+    public function getVars(): array
+    {
+        return $this->data ?: [];
+    }
+}
---
+++ src/Structs/Struct.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ShopEngine\Nova\Structs;
+
+abstract class Struct
+{
+    public static function createFrom(Struct $object)
+    {
+        try {
+            $self = (new \ReflectionClass(static::class))
+                ->newInstanceWithoutConstructor();
+        } catch (\ReflectionException $exception) {
+            throw new \InvalidArgumentException($exception->getMessage());
+        }
+
+        foreach ($object->getVars() as $property => $value) {
+            $self->$property = $value;
+        }
+
+        return $self;
+    }
+
+    public function assign(array $options) : Struct
+    {
+        foreach ($options as $key => $value) {
+            if ($key === 'id' && method_exists($this, 'setId')) {
+                $this->setId($value);
+                continue;
+            }
+
+            try {
+                $this->$key = $value;
+            } catch (\Exception $error) {
+                // @todo maybe more later
+                throw $error;
+            }
+        }
+
+        return $this;
+    }
+
+    public function getVars(): array
+    {
+        return get_object_vars($this);
+    }
+}
