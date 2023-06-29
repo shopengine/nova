41e6447f2095c5a458b21a48c3c25916633b4ae2
Faisal Khan <faisalfazily@gmail.com>
2023-06-29 16:20:07 +0200

    Fix typo

--- src/Models/CodelessModel.php
+++ src/Models/CodelessModel.php
@@ -2,10 +2,9 @@
 
 namespace ShopEngine\Nova\Models;
 
-use SSB\Api\Model\Code;
 use SSB\Api\Model\Codeless;
 
 class CodelessModel extends ShopEngineModel
 {
-    public static $apiModel = Code::class;
+    public static $apiModel = Codeless::class;
 }
